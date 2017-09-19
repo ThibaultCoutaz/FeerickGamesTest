e6e12b887c4b7eb383b7336813e5023c6e3df85c
DESKTOP-TVT6CL4\El Banana Master <pub.coutaz@hotmail.fr>
2017-09-19 11:44:38 +0200

    Simple telechargement de la texture et sauvegarde sur le mobile. Si la texture a déja été téléchargé alors pas de téléchargement et juste utilisation de celle deja enregistré

---
+++ Assets/Scripts/BehaviourCube.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class BehaviourCube : MonoBehaviour {
+
+    public string URL;
+    public Text info;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        info.text = "Start";
+        if (File.Exists(Application.persistentDataPath + "TestTexture.jpg"))
+        {
+            info.text = "Don't need Downloaded";
+            byte[] byteArray = File.ReadAllBytes(Application.persistentDataPath + "TestTexture.jpg");
+            Texture2D texture = new Texture2D(8,8);
+            texture.LoadImage(byteArray);
+            this.GetComponent<Renderer>().material.mainTexture = texture;
+        }
+        else
+        {
+            info.text = "Downloading...";
+            WWW www = new WWW(URL);
+            yield return www;
+            Texture2D texture = www.texture;
+            this.GetComponent<Renderer>().material.mainTexture = texture;
+
+            byte[] bytes = texture.EncodeToJPG();
+            File.WriteAllBytes(Application.persistentDataPath + "TestTexture.jpg", bytes);
+
+            info.text = "Downloaded.";
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
