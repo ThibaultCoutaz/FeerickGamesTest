bfaa3a67a0e31b215d7fbdd728df062d9426ae6f
DESKTOP-TVT6CL4\El Banana Master <pub.coutaz@hotmail.fr>
2017-09-20 19:10:35 +0200

    - le script "ManagerTexture" n'est pas valide et j'étais partie dans trop de direction j'ai donc recommencé dans le script "RessourceManager". mais je le garde pour ne pas perdre les recherche que j'ai fait. - pour le moment je ne travail que avec une image pour rendre fonctionnelle le Thread plus facillement - le thread s'occupe de l'enregistrement et une partie du chargement de l'image (car il reste le LoadImage à modifier et mettre dans un Thread (il n'est pas possible de le mettre tel quel car c'est une fonction de Unity)
    conclusion pour l'instant le programme télécharge une image si ce n'est pas deja fait , puis l'enregistre et la charge ou alors la charge directement (seulement à moitié dans un Thread) .

---
+++ Assets/Scripts/ManagerTexture.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using System.Threading;
+
+public class ManagerTexture : MonoBehaviour {
+
+    /// <summary>
+    /// Script qui n'est plus utilisé et avec beaucoup d'erreurs , juste utilisé pour exemple pour certaines parties du code
+    /// </summary>
+    [System.Serializable]
+    public struct Objects
+    {
+        public MeshRenderer tex;
+        public string URL;
+        [HideInInspector]
+        public string pathSave;
+        [HideInInspector]
+        public byte[] saveData;
+        [HideInInspector]
+        public bool DataReady;
+    }
+
+    public Objects[] listObjects;
+    private List<Objects> listObjectsManage;
+
+    public Text info;
+    public int nbThread;
+
+    #region Private Value
+    private Thread[] listThread;
+    //private Texture2D[] useTexThread;
+    private Texture2D tmpTexture;
+    private WWW[] www;
+    private int indexTexture = 0;
+    private int i;
+    private Objects tmp;
+    #endregion
+
+    IEnumerator Start()
+    {
+        info.text = "Init";
+        listThread = new Thread[nbThread];
+        //useTexThread = new Texture2D[nbThread];
+        listObjectsManage = new List<Objects>();
+
+        www = new WWW[listObjects.Length];
+
+        for(i = 0; i < listObjects.Length; i++)
+        {
+            listObjects[i].pathSave = Application.persistentDataPath + indexTexture + "Texture.jpg";
+            listObjects[i].DataReady = false;
+            listObjectsManage.Add(listObjects[i]);
+        }
+
+        tmpTexture = new Texture2D(8, 8);
+
+        for (i = 0; i < nbThread; i++)
+        {
+            info.text = "Downloading Texture "+i;
+            //Seulement dans le cas ou le nb de thread = le nombre dimage a gerer .
+            www[i] = new WWW(listObjects[i].URL);
+            yield return www[i];
+            
+            listThread[i] = new Thread(() => ThreadManagePic(indexTexture,i));
+            listThread[i].Start();
+            indexTexture++;
+
+
+        }
+    }
+
+    // Use this for initialization
+    //   IEnumerator Start () {
+    //       info.text = "Start";
+    //       for (int i = 0; i < listObjects.Length; i++)
+    //       {
+    //           if (File.Exists(Application.persistentDataPath + i + "Texture.jpg"))
+    //           {
+    //               info.text = "Don't need Downloaded";
+    //               byte[] byteArray = File.ReadAllBytes(Application.persistentDataPath + i + "Texture.jpg");
+    //               Texture2D texture = new Texture2D(8, 8);
+    //               texture.LoadImage(byteArray);
+    //               listObjects[i].tex.material.mainTexture = texture;
+    //           }
+    //           else
+    //           {
+    //               info.text = "Downloading...";
+    //               WWW www = new WWW(listObjects[i].URL);
+    //               yield return www;
+    //               Texture2D texture = www.texture;
+    //               listObjects[i].tex.material.mainTexture = texture;
+
+    //               byte[] bytes = texture.EncodeToJPG();
+    //               File.WriteAllBytes(Application.persistentDataPath + i + "Texture.jpg", bytes);
+
+    //               info.text = "Downloaded.";
+    //           }
+    //       }
+    //}
+
+    // Update is called once per frame
+
+    void Update () {
+        if(listObjectsManage.Count > 0)
+        {
+            for(int i=0; i < listObjectsManage.Count; i++)
+            {
+                Debug.LogError(listObjectsManage[i].DataReady+" -- "+ listObjects[i].DataReady);
+                if (listObjectsManage[i].DataReady)
+                {
+                    Debug.LogError("loading texture "+i);
+                    tmp = listObjectsManage[i];
+                    tmpTexture.LoadImage(tmp.saveData);
+                    tmp.tex.material.mainTexture = tmpTexture;
+                    tmp.DataReady = false;
+                }
+            }
+        }
+    }
+
+    private void ThreadManagePic(int index,int indexThread)
+    {
+        if (File.Exists(listObjects[index].pathSave))
+        {
+            Debug.LogError("image " + index + " exsiste");
+            listObjectsManage.ToArray()[index].saveData = File.ReadAllBytes(listObjects[index].pathSave);
+            //useTexThread[indexThread].LoadImage(byteArray);
+            //listObjects[index].tex.material.mainTexture = useTexThread[indexThread];
+            listObjectsManage.ToArray()[index].DataReady = true;
+        }
+        else
+        {
+            Debug.LogError("image " + index + " n'exsiste pas");
+        }
+    }
+    
+}
---
+++ Assets/Scripts/RessourceManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using System.Threading;
+
+public class RessourceManager : MonoBehaviour {
+
+    public Text info;
+
+    [System.Serializable]
+    public struct Objects
+    {
+        public MeshRenderer tex;
+        public string URL;
+        [HideInInspector]
+        public string pathSave;
+        [HideInInspector]
+        public byte[] saveData;
+        [HideInInspector]
+        public bool DataReady;
+    }
+
+    public Objects listObjects;
+    private WWW www;
+    private Thread listThread;
+    private Texture2D texture;
+
+    // Use this for initialization
+    IEnumerator Start () {
+
+        texture = new Texture2D(8, 8);
+
+        //www = new WWW[listObjects.Length];
+
+
+        //for (int i=0; i< listObjects.Length; i++)
+        //{
+            listObjects.DataReady = false;
+            listObjects.pathSave = Application.persistentDataPath + /*i +*/ "Texture.jpg"; //Still have to find a better way because if you change the order in the inspector or just change the place of one element he will take the texture of the old one
+            if (File.Exists(listObjects.pathSave))
+            {
+                info.text = "Dont Need to Download";
+                listThread = new Thread(() => LoadTexture()); //Pour les paramétres plus tard
+                listThread.Start();
+            }
+            else
+            {
+                info.text = "Downloading";
+                www = new WWW(listObjects.URL);
+                yield return www;
+                texture = www.texture;
+
+                listThread = new Thread(() => SaveTexture()); //Pour les paramétres plus tard
+                listThread.Start();
+            }
+
+        //}	
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (listObjects.DataReady)
+        {
+            texture.LoadImage(listObjects.saveData);
+            listObjects.tex.material.mainTexture = texture;
+        }
+    }
+
+    private void SaveTexture()
+    {
+        info.text = "Texture sauvegardé";
+        byte[] bytes = texture.EncodeToJPG();
+        File.WriteAllBytes(listObjects.pathSave, bytes);
+        LoadTexture();
+    }
+
+    private void LoadTexture()
+    {
+        //info.text = "Loading Texture";
+        listObjects.saveData = File.ReadAllBytes(listObjects.pathSave);
+        listObjects.DataReady = true;
+    }
+}
